15e0d8ff5cbd827cddd472eda5ecf839a1f54412
Alex Zylman <alex@zylman.com>
2012-06-05 13:48:27 -0500

    add more events

--- js/main.js
+++ js/main.js
@@ -10,22 +10,58 @@ var recommendations = [
 		image : 'http://upload.wikimedia.org/wikipedia/commons/a/a8/Northwestern_Arch.jpg',
 		ticket : '#',
 		price: 'free'
+	},
+	{
+		title  : 'Shedd Aquarium',
+		start  : '2012-06-09 13:00:00',
+		end  : '2012-06-09 15:00:00',
+		allDay : false,
+		desc : "Come see fishies and sea creatures of all kinds - including 25 different species of sharks - at Shedd, Chicago's famous aquarium. You get access to the Ocenarium Show and Exhibits, the general aquarium, the Wild Reef, and the Polar Play Zone. Buses will be provided to and from campus for this event.",
+		times : 'Buses leave at 1:00 PM',
+		location : 'John G. Shedd Aquarium, 1200 South Lake Shore Drive, Chicago, IL',
+		image : 'http://static.ddmcdn.com/gif/family-vacations-shedd-aquarium.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$12'
+	},
+	{
+		title  : 'Lincoln Park Zoo',
+		start  : '2012-06-09 13:00:00',
+		allDay : false,
+		end  : '2012-06-09 15:00:00',
+		desc : "Who doesn't love the zoo - seriously. After four years of stressing out over classes, senior theses, and whether or not we'll be employed next year, it's time to act like a kid again and make faces at the monkeys. Buses will be provided to and from campus for this event, but feel free to stay in the area and enjoy the great restaurants in the Lincoln Park West neighborhood.",
+		times : 'Buses leave at 1:00 PM',
+		location : 'Lincoln Park Zoo, 2200 North Cannon Drive, Chicago, IL',
+		image : 'http://www.chicagonow.com/lists-that-actually-matter/files/2011/07/PICT0392_medium.jpg',
+		ticket : '#',
+		price: 'free'
+	},
+	{
+		title  : 'Kingston Mines Blues Club',
+		start  : '2012-06-09 22:00:00',
+		end  : '2012-06-09 24:00:00',
+		allDay : false,
+		desc : "Two stages in adjoining rooms present nonstop blues all night long at Kingston Mines, the great granddaddy of the Chicago music scene. The only blues bar with a 4am license, their motto is 'hear the blues, drink the booze, and talk loud.' Get in with special $10 discounted cover if you show your WildCard!",
+		times : '10:00 PM',
+		location : '2548 N. Halsted Street, Chicago, IL',
+		image : 'http://www.chibarproject.com/Reviews/KingstonMines/KingstonMinesCoupleYC.jpg',
+		ticket : '#',
+		price: '$10'
+	},
+	{
+		title  : 'Ask Big Questions Bonfire and BBQ',
+		start  : '2012-06-10 19:00:00',
+		end  : '2012-06-10 21:00:00',
+		allDay : false,
+		desc : "Senior Week is excited to partner with Ask Big Questions (ABQ) for a night of s'mores, BBQ, and deep conversations. Reminisce about the past four years and freak-out about the future. ABQ aims to discuss life's big questions in a secular, casual environment. BYOMRS (Bring your own marshmallow-roasting stick).",
+		times : '7:00 PM',
+		location : 'The Lakefill fire pit',
+		image : 'http://images.businessweek.com/ss/09/08/0811_northwestern_school_tour/image/lakefill.jpg',
+		ticket : '#',
+		price: 'free'
 	}
 ];
 
 var sponsoredRecommendations = [
-	{
-		title  : 'Goose Island Private Tour and Tasting',
-		start  : '2012-06-10 11:30:00',
-		end  : '2012-06-10 14:00:00',
-		allDay : false,
-		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
-		times : '11:30AM, 2PM, and 4PM',
-		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
-		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
-		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
-		price: '$13'
-	},
 	{
 		title  : 'Senior Class Gift Reception',
 		start  : '2012-06-08 18:00:00',
@@ -35,6 +71,78 @@ var sponsoredRecommendations = [
 		times : '6:00 PM',
 		location : 'The Lakefill - invitation only',
 		image : 'http://images.businessweek.com/ss/09/08/0811_northwestern_school_tour/image/lakefill.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$13'
+	},
+	{
+		title  : 'Goose Island Private Tour and Tasting',
+		start  : '2012-06-10 11:30:00',
+		end  : '2012-06-10 13:00:00',
+		allDay : false,
+		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
+		times : '11:30AM, 2PM, and 4PM',
+		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
+		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$13'
+	},
+	{
+		title  : 'Goose Island Private Tour and Tasting',
+		start  : '2012-06-10 14:00:00',
+		end  : '2012-06-10 15:30:00',
+		allDay : false,
+		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
+		times : '11:30AM, 2PM, and 4PM',
+		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
+		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$13'
+	},
+	{
+		title  : 'Goose Island Private Tour and Tasting',
+		start  : '2012-06-10 16:00:00',
+		end  : '2012-06-10 17:30:00',
+		allDay : false,
+		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
+		times : '11:30AM, 2PM, and 4PM',
+		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
+		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$13'
+	},
+	{
+		title  : 'Goose Island Private Tour and Tasting',
+		start  : '2012-06-11 11:30:00',
+		end  : '2012-06-11 13:00:00',
+		allDay : false,
+		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
+		times : '11:30AM, 2PM, and 4PM',
+		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
+		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$13'
+	},
+	{
+		title  : 'Goose Island Private Tour and Tasting',
+		start  : '2012-06-11 14:00:00',
+		end  : '2012-06-11 15:30:00',
+		allDay : false,
+		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
+		times : '11:30AM, 2PM, and 4PM',
+		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
+		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
+		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
+		price: '$13'
+	},
+	{
+		title  : 'Goose Island Private Tour and Tasting',
+		start  : '2012-06-11 16:00:00',
+		end  : '2012-06-11 17:30:00',
+		allDay : false,
+		desc : "Perhaps the most popular event from previous Senior Weeks, we are excited to once again offer private tours of Chicago's own Goose Island Brewery. Get your ticket ASAP for a 90 minute tour through the famous Chicago brewery and sample some of the finest ales in all the land. Our event is being held as Goose Island's Clybourn location, which is just off the Red Line, so there will be no buses for this event. This event is 21+.",
+		times : '11:30AM, 2PM, and 4PM',
+		location : 'Goose Island Brewing Co., 1800 North Clybourn Avenue, Chicago, IL',
+		image : 'http://www.gooseisland.com/goosefilebin/images/clybourn3.jpg',
 		ticket : 'http://nbo.universitytickets.com/user_pages/category.asp?id=34',
 		price: '$13'
 	}
